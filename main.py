8724ad8957d62bad55456fe1733491e0ea710ca7
Eshaan Parlikar <parlikareshaan@gmail.com>
2020-11-17 22:03:27 -0800

    Changed IP and Port

--- main.py
+++ main.py
@@ -81,4 +81,4 @@ def gametest():
   return render_template("gametest.html")
 
 if __name__ == "__main__":
-  app.run(debug=True, port='3000', host='127.0.0.1')
+  app.run(debug=True, host='0.0.0.0',port='80')
